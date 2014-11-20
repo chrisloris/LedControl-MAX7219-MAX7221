9f0f91d508d4034987d7929ba2366e4867b38893
chrisloris <christian.loris@gmail.com>
2014-11-19 20:33:41 -0500

    Fixing Items for Spark Library Inclusion

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ LedControl
 ==========
 
 Port of Eberhard Fahle's LedControl library for controlling the MAX7219/MAX7221 for the Spark Core.
+
+https://code.google.com/p/arudino-maxmatrix-library/
---
+++ examples/matrixpaneldemo.ino
@@ -0,0 +1,36 @@
+// This #include statement was automatically added by the Spark IDE.
+#include "LedControl.h"
+
+LedControl *led;
+int phase = 0;
+char message[64];
+int messageLength = 0;
+int myUptime = 0;
+
+uint8_t data = A5;
+uint8_t load = A4;
+uint8_t myclock = A3;
+
+void setup() {
+    led = new LedControl(data,myclock,load,1); //DIN,CLK,CS,HowManyDisplays
+    led-> shutdown(0,false); //Turn it on
+    led-> setIntensity(0,1);
+}
+
+void loop() {
+
+    if(phase==0){ //Message-loop starts
+        myUptime = (int)(millis()/1000);
+        sprintf(message,"Uptime %d:%02d", myUptime / 3600, (myUptime % 3600)/60); //update message
+        messageLength = strlen(message); // ...and length
+        led->tweenLetters(0,' ',message[phase]); //scroll from empty to 1 letter
+    }
+
+    if(phase<messageLength-1){ //next characters except last one
+        led->tweenLetters(0,message[phase],message[phase+1]);
+        phase++;
+    }else if(phase==messageLength-1){//last character scrolls to empty
+        led->tweenLetters(0,message[phase],' ');
+        phase = 0; //restart message-loop
+    }
+}
